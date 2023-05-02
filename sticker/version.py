b01d6f0becfab345ee6927da403bb777c9ee3629
ImSanae <dev@sanae.im>
2023-05-02 17:39:34 +0000

    added stickers

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '3cb8276c'
-version = '0.1.0+dev.3cb8276c'
-linkified_version = '0.1.0+dev.[3cb8276c](https://github.com/maunium/stickerpicker/commit/3cb8276c44c08f7ecbde5138011de6b06141e198)'
+git_revision = '066b4354'
+version = '0.1.0+dev.066b4354'
+linkified_version = '0.1.0+dev.[066b4354](https://github.com/maunium/stickerpicker/commit/066b4354e97d7eca71e2478b70e6b2c7f5489ca0)'
